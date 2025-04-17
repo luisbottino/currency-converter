56e86e35a5b0fb293fab56b4bff25db99e35cc11
luisbottino <luzbottino@gmail.com>
2025-04-16 22:41:46 -0300

    add integration tests for constraint violations

--- src/test/kotlin/br/com/luisbottino/controller/v1/CurrencyConversionControllerV1IntegrationTest.kt
+++ src/test/kotlin/br/com/luisbottino/controller/v1/CurrencyConversionControllerV1IntegrationTest.kt
@@ -77,5 +77,27 @@ class CurrencyConversionControllerV1IntegrationTest
             .andExpect(status().isNotFound)
     }
 
+    @Test
+    fun givenInvalidPage_whenGetConversionHistory_thenReturnBadRequest() {
+        mockMvc.perform(
+            get(TestData.PATH_API_V1_CONVERSIONS.plus(TestData.DEFAULT_USER_ID))
+                .queryParam(TestData.PAGE_QUERY_PARAM, "-1")
+                .queryParam(TestData.SIZE_QUERY_PARAM, TestData.DEFAULT_PAGE_SIZE.toString())
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isBadRequest)
+            .andExpect(jsonPath("$.message").value("The page number must not be negative."))
+    }
 
+    @Test
+    fun givenInvalidSize_whenGetConversionHistory_thenReturnBadRequest() {
+        mockMvc.perform(
+            get(TestData.PATH_API_V1_CONVERSIONS.plus(TestData.DEFAULT_USER_ID))
+                .queryParam(TestData.PAGE_QUERY_PARAM, TestData.DEFAULT_PAGE_NUMBER.toString())
+                .queryParam(TestData.SIZE_QUERY_PARAM, "0")
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isBadRequest)
+            .andExpect(jsonPath("$.message").value("The page size must be at least 1."))
+    }
 }
