bea5bb8d008461675e99183b8e1496c14d56c624
luisbottino <luzbottino@gmail.com>
2025-04-16 22:40:50 -0300

    add validations to conversion history endpoint

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-logging")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
 
 
     //DB
--- src/main/kotlin/br/com/luisbottino/controller/v1/CurrencyConversionControllerV1.kt
+++ src/main/kotlin/br/com/luisbottino/controller/v1/CurrencyConversionControllerV1.kt
@@ -1,10 +1,13 @@
 package br.com.luisbottino.controller.v1
 
 import br.com.luisbottino.service.ConversionService
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Slice
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam
 
 @RestController
 @RequestMapping("/api/v1/conversions")
+@Validated
 class CurrencyConversionControllerV1(
     private val conversionService: ConversionService
 ) {
@@ -21,9 +25,15 @@ class CurrencyConversionControllerV1(
 
     @GetMapping("/{userId}")
     fun getConversionHistory(
-        @PathVariable userId: String,
-        @RequestParam(defaultValue = "0") page: Int,
-        @RequestParam(defaultValue = "10") size: Int,
+        @PathVariable
+        @NotBlank(message = "The userId must not be blank or null.")
+        userId: String,
+        @RequestParam(defaultValue = "0")
+        @Min(value = 0, message = "The page number must not be negative.")
+        page: Int,
+        @RequestParam(defaultValue = "10")
+        @Min(value = 1, message = "The page size must be at least 1.")
+        size: Int,
     ): Slice<ConversionHistoryResponse> {
         logger.info("Received request to fetch conversion history for userId={}, page={}, size={}", userId, page, size)
         val pageable = PageRequest.of(page, size)
